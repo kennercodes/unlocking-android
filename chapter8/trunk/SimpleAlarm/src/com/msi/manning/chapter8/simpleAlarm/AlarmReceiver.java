6c012e5e5231ef7be9c358e084d4a351f788085d
robisen@gmail.com <robisen@gmail.com@9c30c16f-5055-0410-a2a9-c9875879f44b>
2009-01-26 01:09:49 +0000

    update for final

--- chapter8/trunk/SimpleAlarm/src/com/msi/manning/chapter8/simpleAlarm/AlarmReceiver.java
+++ chapter8/trunk/SimpleAlarm/src/com/msi/manning/chapter8/simpleAlarm/AlarmReceiver.java
@@ -1,21 +1,18 @@
 package com.msi.manning.chapter8.simpleAlarm;
 
-
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.BroadcastReceiver;
 import android.widget.Toast;
 
-public class AlarmReceiver extends BroadcastReceiver  {
-	
-    public void onReceiveIntent(Context context, Intent intent)
-    {
-        Toast.makeText(context, R.string.app_name, Toast.LENGTH_SHORT).show();
-}
+public class AlarmReceiver extends BroadcastReceiver {
 
-	@Override
-	public void onReceive(Context context, Intent intent) {
-		// TODO Auto-generated method stub
-		
-	}
+    public void onReceiveIntent(Context context, Intent intent) {
+        Toast.makeText(context, R.string.app_name, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
 }
--- chapter8/trunk/SimpleAlarm/src/com/msi/manning/chapter8/simpleAlarm/GenerateAlarm.java
+++ chapter8/trunk/SimpleAlarm/src/com/msi/manning/chapter8/simpleAlarm/GenerateAlarm.java
@@ -1,8 +1,11 @@
 package com.msi.manning.chapter8.simpleAlarm;
 
+import java.util.Calendar;
+
 import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,42 +13,39 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Toast;
 
-import java.util.Calendar;
+public class GenerateAlarm extends Activity {
 
-public class GenerateAlarm extends Activity
-{
     Toast mToast;
 
     @Override
-	protected void onCreate(Bundle icicle)
-    {
+    protected void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         setContentView(R.layout.main);
-        Button button = (Button)findViewById(R.id.set_alarm_button);
-        button.setOnClickListener(mOneShotListener);
-     }
+        Button button = (Button) findViewById(R.id.set_alarm_button);
+        button.setOnClickListener(this.mOneShotListener);
+    }
 
-    private OnClickListener mOneShotListener = new OnClickListener()
-    {
-        public void onClick(View v)
-        {
-            
+    private OnClickListener mOneShotListener = new OnClickListener() {
+
+        public void onClick(View v) {
+
             Intent intent = new Intent(GenerateAlarm.this, AlarmReceiver.class);
 
             PendingIntent appIntent = PendingIntent.getBroadcast(GenerateAlarm.this, 0, intent, 0);
-            
+
             Calendar calendar = Calendar.getInstance();
             calendar.setTimeInMillis(System.currentTimeMillis());
             calendar.add(Calendar.SECOND, 30);
 
-            AlarmManager am = (AlarmManager)getSystemService(ALARM_SERVICE);
+            AlarmManager am = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
             am.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), appIntent);
 
-            if (mToast != null) {mToast.cancel();
+            if (GenerateAlarm.this.mToast != null) {
+                GenerateAlarm.this.mToast.cancel();
             }
-            mToast = Toast.makeText(GenerateAlarm.this, R.string.alarm_message, Toast.LENGTH_LONG);
-            mToast.show();
+            GenerateAlarm.this.mToast = Toast.makeText(GenerateAlarm.this, R.string.alarm_message, Toast.LENGTH_LONG);
+            GenerateAlarm.this.mToast.show();
         }
     };
-    
+
 }
